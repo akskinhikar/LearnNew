d308fe8a6c0bdec95a2ac3f0588dfc0c4b88928f
akskinhikar <akskinhikar@gmail.com>
2024-12-20 14:31:23 -0500

    secondpush

--- mavenorg/src/test/java/org/sample/mavenorg/DesiredCap.java
+++ mavenorg/src/test/java/org/sample/mavenorg/DesiredCap.java
@@ -1,6 +1,14 @@
 package org.sample.mavenorg;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,9 +17,31 @@ public class DesiredCap {
 	@Test 
 	public void OpenBrowser() {
 		WebDriverManager.chromiumdriver().setup();
-		ChromeDriver driver = new ChromeDriver();
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.acceptInsecureCerts();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("disable-infobars");
+		options.merge(caps);
+		WebDriver driver = new ChromeDriver(options);
+		
+		//Code for Select Class
+		
+		WebElement test1 = driver.findElement(By.xpath(""));
+		Select sel = new Select(test1);
+		sel.selectByIndex(0);
+		
+		
+		//Code for Actions Class
+		
+		WebElement test = driver.findElement(By.xpath(""));
+		Actions builder = new Actions(driver);
+		Action moveOverHome = builder.moveToElement(test).build();
+		moveOverHome.perform();
+		
 		
 		driver.get("https://google.com");
+		driver.quit();
 	}
 
 }
---
+++ mavenorg/src/test/java/org/sample/mavenorg/TestNGDetails.java
@@ -0,0 +1,64 @@
+package org.sample.mavenorg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.beust.jcommander.Parameter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import junit.framework.Assert;
+
+@Listeners(org.sample.mavenorg.ListenerTest.class)
+public class TestNGDetails {
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class Method");
+	}
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After Class Method");
+	}
+	
+	@BeforeMethod
+	public void beforGroup() {
+		System.out.println("Before Method");
+	}
+	
+	@Test (description = "Method for opening browser")
+	public void openBrowser() {
+		SoftAssert sft = new SoftAssert();
+		sft.assertFalse(true);
+		sft.assertAll();
+	System.out.println("Open Browser");
+	}
+	
+	@Test (dependsOnMethods = {"openBrowser"})
+	public void launchDriver(@Optional() String method) {
+	//System.out.println("Launch Driver");
+		Assert.assertFalse(false);
+		
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("-- After Method --");
+	}
+
+}
